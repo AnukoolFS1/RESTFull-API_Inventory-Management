0c53acf38ca05c6285dc3a207fddde00d8ca3e9d
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-12-31 16:43:30 +0530

    try catch for creating a product

--- controller/product.control.js
+++ controller/product.control.js
@@ -3,16 +3,21 @@ const Categorymodel = require('../models/Category');
 const Suppliermodel = require('../models/Supplier');
 
 const addProduct = async (req, res) => {
-    const { name, price, quantity, category, supplier } = req.body;
-    const categoryId = await Categorymodel.findOne({ name: category }).select("_id");
-    const supplierId = await Suppliermodel.findOne({ companyName: supplier }).select("_id");
-    if(!supplierId && !categoryId) return res.status(400).json({msg:"Supplier or Category are not available right now"})
-    const product = new Productmodel({
+    try{
+        const { name, price, quantity, category, supplier } = req.body;
+        const categoryId = await Categorymodel.findOne({ name: category }).select("_id");
+        const supplierId = await Suppliermodel.findOne({ companyName: supplier }).select("_id");
+        if(!supplierId && !categoryId) return res.status(400).json({msg:"Supplier or Category are not available right now"})
+        const product = new Productmodel({
         name, price, quantity, categoryId: categoryId._id, supplierId: supplierId._id
-    })
-
-    await product.save();
-    res.status(201).json({ msg: "Product has created successfully" })
+        }) 
+    
+        await product.save();
+        res.status(201).json({ msg: "Product has created successfully" })
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({msg:"Internal server error"})
+    }
 }
 
 const getProducts = async (req, res) => {
